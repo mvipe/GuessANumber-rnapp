5d542b682ea08e600711d14a3bd4715dcc43bcbd
mohnish vats <mohnishvats@mohnishs-MacBook-Air.local>
2021-10-06 22:38:31 +0530

    extracting card component and use it in Start Game Screen

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { View, StyleSheet, Text, TextInput, Button } from "react-native";
+import Card from "../components/Card";
 
 const StartGameScreen = (props) => {
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Start a New Game!</Text>
-      <View style={styles.inputContainer}>
+      <Card styles={styles.inputContainer}>
         <Text>Select a Number</Text>
         <TextInput />
         <View style={styles.buttonContainer}>
           <Button title="Reset" />
           <Button title="Confirm" />
         </View>
-      </View>
+      </Card>
     </View>
   );
 };
@@ -37,14 +38,6 @@ const styles = StyleSheet.create({
     width: 300,
     maxWidth: "80%",
     alignItems: "center",
-    shadowColor: "black",
-    shadowOffset: { width: 0, height: 2 },
-    shadowRadius: 6,
-    shadowOpacity: 0.26,
-    backgroundColor: "white",
-    elevation: 5,
-    padding: 20,
-    borderRadius: 10,
   },
 });
 
