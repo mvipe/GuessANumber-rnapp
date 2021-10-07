32d9ed03406137108379ede552e7125fa403798e
mohnish vats <mohnishvats@mohnishs-MacBook-Air.local>
2021-10-07 19:33:11 +0530

    added Game Screen and added random generation function

---
+++ screens/GameScreen.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  TextInput,
+  Button,
+  TouchableWithoutFeedback,
+  Keyboard,
+  Alert,
+} from "react-native";
+
+const generateRandomBetween = (min, max, exclude) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const rndNum = Math.floor(Math.random * (max - min) + min);
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+};
+
+const GameScreen = (props) => {
+  const [currentGuess, setCurrentGuess] = useState(
+    generateRandomBetween(1, 100, props.userChoice)
+  );
+
+  return <View></View>;
+};
+
+const styles = StyleSheet.create({});
+
+export default GameScreen;
