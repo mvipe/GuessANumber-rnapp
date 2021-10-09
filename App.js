13fcfcad8cac5f9bdf86a15d4f0cb4a609b82ed2
mohnish vats <mohnishvats@mohnishs-MacBook-Air.local>
2021-10-09 16:05:31 +0530

    implemented custom fonts with AppLoading

--- App.js
+++ App.js
@@ -5,10 +5,35 @@ import Header from "./components/Header";
 import GameOver from "./screens/GameOver";
 import GameScreen from "./screens/GameScreen";
 import StartGameScreen from "./screens/StartGameScreen";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+
+const fetchFont = () => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf"),
+  });
+};
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [guessRound, setGuessRound] = useState(0);
+
+  const [dataLoaded, setDataLoaded] = useState(false);
+
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFont}
+        onFinish={() => {
+          setDataLoaded(true);
+        }}
+        onError={(err) => {
+          console.log(err);
+        }}
+      />
+    );
+  }
 
   const configureNewGameHandler = () => {
     setGuessRound(0);
